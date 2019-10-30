bf124c63a8bf34e7c49b1b44b15e96f98e18de00
Sara <gasoline898@outlook.com>
2019-10-30 21:25:15 +0100

    Add number, History fragment, delete seizure record,and swiperefresh.

--- app/src/main/java/com/sas/epilepstop/ui/ContactsFragment.java
+++ app/src/main/java/com/sas/epilepstop/ui/ContactsFragment.java
@@ -2,12 +2,15 @@ package com.sas.epilepstop.ui;
 
 import android.app.Activity;
 import android.app.Fragment;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.PersistableBundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
@@ -25,10 +28,77 @@ import io.objectbox.Box;
 
 public class ContactsFragment extends android.support.v4.app.Fragment {
 
+    Box<Contacts> contactsBox;
+    ContactsCustomAdapter adapter;
+    ListView listview_contacts;
+    Context context;
+    SwipeRefreshLayout mSwipeRefreshLayout;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_contacts, container, false);
+        View view =  inflater.inflate(R.layout.fragment_contacts, container, false);
+
+        context = getActivity().getApplicationContext();
+
+
+        contactsBox = ObjectBox.get().boxFor(Contacts.class);
+
+        adapter = new ContactsCustomAdapter(context, contactsBox.getAll());
+
+        listview_contacts = view.findViewById(R.id.listView_contacts);
+        listview_contacts.setAdapter(adapter);
+        registerForContextMenu(listview_contacts);
+
+
+        mSwipeRefreshLayout = view.findViewById(R.id.contacts_tab_swipe);
+        mSwipeRefreshLayout.setColorSchemeResources(R.color.purpleveryLight, R.color.purpleLight, R.color.purpleDark);
+
+
+        mSwipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                //handling swipe refresh
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        mSwipeRefreshLayout.setRefreshing(false);
+                        listview_contacts.setAdapter(null);
+                        adapter = new ContactsCustomAdapter(context, contactsBox.getAll());
+                        listview_contacts.setAdapter(adapter);
+                        adapter.notifyDataSetChanged();
+                        listview_contacts.smoothScrollToPosition(0);
+                    }
+                }, 1500);
+            }
+        });
+
+
+
+
+        EditText entered_number = view.findViewById(R.id.enter_contact);
+
+        Button btn_ok  = view.findViewById(R.id.btn_ok);
+        btn_ok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if(!entered_number.getText().toString().equals("")) {
+                    Contacts contact = new Contacts();
+                    contact.setNumber(entered_number.getText().toString());
+                    contactsBox.put(contact);
+                    entered_number.setText("");
+
+                }
+
+            }
+        });
+
+
+
+
+
+        return view;
     }
 
 }
--- app/src/main/java/com/sas/epilepstop/ui/HistoryFragment.java
+++ app/src/main/java/com/sas/epilepstop/ui/HistoryFragment.java
@@ -1,11 +1,14 @@
 package com.sas.epilepstop.ui;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.ContextMenu;
@@ -28,10 +31,82 @@ import java.util.List;
 import io.objectbox.Box;
 
 public class HistoryFragment extends Fragment {
+
+    ListView listview_seizures;
+    Box<Seizure> seizureBox;
+    CustomAdapter adapter;
+    Context context;
+    SwipeRefreshLayout mSwipeRefreshLayout;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_history, container, false);
+        View view = inflater.inflate(R.layout.fragment_history, container, false);
+
+        context = getActivity().getApplicationContext();
+        seizureBox = ObjectBox.get().boxFor(Seizure.class);
+
+        adapter = new CustomAdapter(context, seizureBox.getAll());
+        listview_seizures = view.findViewById(R.id.listView);
+        listview_seizures.setAdapter(adapter);
+        registerForContextMenu(listview_seizures);
+
+
+        mSwipeRefreshLayout = view.findViewById(R.id.history_swipe);
+        mSwipeRefreshLayout.setColorSchemeResources(R.color.purpleveryLight, R.color.purpleLight, R.color.purpleDark);
+
+
+        mSwipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                //handling swipe refresh
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        mSwipeRefreshLayout.setRefreshing(false);
+                        listview_seizures.setAdapter(null);
+                        adapter = new CustomAdapter(context, seizureBox.getAll());
+                        listview_seizures.setAdapter(adapter);
+                        adapter.notifyDataSetChanged();
+                        listview_seizures.smoothScrollToPosition(0);
+                    }
+                }, 1500);
+            }
+        });
+
+
+
+        return view;
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater inflater = getActivity().getMenuInflater();
+        inflater.inflate(R.menu.contextmenu_listview,menu);
+
+
+        menu.getItem(0).setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+               // int index = item.getItemId();
+
+                AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+                int index = info.position;
+
+                //if (item.ti == "Delete") {
+                    Seizure seizure = (Seizure) listview_seizures.getAdapter().getItem(index);
+                    seizureBox.remove(seizure);
+                    adapter.notifyDataSetChanged();
+
+                //}
+
+                return false;
+            }
+
+
+        });
+
     }
 
 }
